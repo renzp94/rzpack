4cc21117d10c7ac8d6c020b875f4f85c0685967c
任振鹏 <1074720760@qq.com>
2024-07-02 13:31:21 +0800

    fix(rzpack): 修复FriendlyErrorsWebpackPlugin在Rspack中无法显示编译时间的问题

--- packages/rzpack/src/common/plugins/friendly-errors-webpack-plugin.ts
+++ packages/rzpack/src/common/plugins/friendly-errors-webpack-plugin.ts
@@ -1,17 +1,40 @@
 import FriendlyErrorsWebpackPlugin from '@soda/friendly-errors-webpack-plugin'
 import type WebpackChain from 'webpack-chain'
 
+class HackerFriendlyErrorsWebpackPlugin extends FriendlyErrorsWebpackPlugin {
+  getStatsCompileTime(stats, statsIndex) {
+    const { endTime, startTime } = stats.compilation
+    if (statsIndex !== undefined) {
+      if (
+        (this as FriendlyErrorsWebpackPlugin).previousEndTimes[statsIndex] ===
+        endTime
+      ) {
+        return 0
+      }
+      ;(this as FriendlyErrorsWebpackPlugin).previousEndTimes[statsIndex] =
+        endTime
+    }
+
+    return endTime - startTime
+  }
+}
+
 export default (webpackChain: WebpackChain) => {
   webpackChain.stats('errors-only').set('infrastructureLogging', {
     level: 'none',
   })
-  webpackChain.plugin('friendly-errors').use(FriendlyErrorsWebpackPlugin, [
-    {
-      clearConsole: true,
-      additionalFormatters: [],
-      additionalTransformers: [],
-    },
-  ])
+  webpackChain
+    .plugin('friendly-errors')
+    .use<typeof FriendlyErrorsWebpackPlugin>(
+      HackerFriendlyErrorsWebpackPlugin,
+      [
+        {
+          clearConsole: true,
+          additionalFormatters: [],
+          additionalTransformers: [],
+        },
+      ],
+    )
 
   return webpackChain
 }
